52b91aabe9062a4bb8da0c586ccfef95e2e0f819
Nathan <namo678@me.com>
2022-09-11 18:53:53 -0400

    commit tree e9d4a627bd41c32760c8a16b3a2064a08862cdba

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,31 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
+  post '/messages' do 
+    message = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    message.to_json
+  end
+
+  patch '/messages/:id' do 
+    message_to_update = Message.find(params[:id])
+    message_to_update.update(
+      body: params[:body]
+    )
+    message_to_update.to_json
+  end
+
+  delete '/messages/:id' do 
+    message_to_delete = Message.find(params[:id])
+    message_to_delete.destroy
+    message_to_delete.to_json
+  end
   
 end
